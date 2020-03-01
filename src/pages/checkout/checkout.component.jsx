d2618cb3c5bd2766d06f850858d7c7e8322ec51b
laurent pollez <laurent.pollez@gmail.com>
2020-03-01 18:18:38 +0100

    updating stripe integration and updating our favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import {
   selectCartItems,
   selectCartTotal
-} from '../../redux/cart/cart.selectors'
+} from '../../redux/cart/cart.selectors';
 
-import { createStructuredSelector } from 'reselect'
+import { createStructuredSelector } from 'reselect';
 
-import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
-import './checkout.styles.scss'
+import './checkout.styles.scss';
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -37,12 +38,18 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL : {total}</span>
     </div>
+    <StripeCheckoutButton price={total} />
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp : 01/20 - CVV: 123
+    </div>
   </div>
-)
+);
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
   total: selectCartTotal
-})
+});
 
-export default connect(mapStateToProps)(CheckoutPage)
+export default connect(mapStateToProps)(CheckoutPage);
